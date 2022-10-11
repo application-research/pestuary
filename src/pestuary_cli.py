69f396ce0a9a1452d0318de7b97c66af67c4c856
mike seiler <snissn@gmail.com>
2022-10-11 11:07:36 -1000

    bugfix for command names filter

--- src/pestuary_cli.py
+++ src/pestuary_cli.py
@@ -2,7 +2,7 @@ import pestuary
 import instantcli
 import json
 
-instantcli.filter_function_name = lambda name: not name.startswith("main") or not name.startswith("_")
+instantcli.filter_function_name = lambda name: not name.startswith("main") and not name.startswith("_")
 instantcli.post_call = lambda result: print(json.dumps(result, default=lambda x: x.to_dict()))
 
 
