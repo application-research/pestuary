91f615aef2c9f62e871cf8c88489c77bf544f87b
mike seiler <snissn@gmail.com>
2022-10-11 11:19:47 -1000

    bump versionnumber

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name="pestuary",
-        version="1.0.4",
+        version="1.0.5",
         package_dir={'':'src'},
         install_requires=[
                 "estuary_client",
