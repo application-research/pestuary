f22ddc30e669db4dd6f9596b9f149a9c7448ef5a
anjor <anjor@umd.edu>
2022-11-10 22:47:03 +0000

    cleanup

--- src/pestuary_cli.py
+++ src/pestuary_cli.py
@@ -1,6 +1,8 @@
-import pestuary
-import instantcli
 import json
+
+import instantcli
+
+import pestuary
 
 instantcli.filter_function_name = lambda name: not name.startswith("main") and not name.startswith("_")
 instantcli.post_call = lambda result: print(json.dumps(result, default=lambda x: x.to_dict()))
