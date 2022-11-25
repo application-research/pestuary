e97c8845752ce2cd61f645b54faf1d8fb1b7815b
anjor <anjor@umd.edu>
2022-11-25 13:11:28 +0000

    refactor add-dir-to-collection

--- examples/add-dir-to-collection.py
+++ examples/add-dir-to-collection.py
@@ -1,45 +1,13 @@
-import os
-import random
-import shutil
-import string
-
 from pestuary.collections import collection_list_content
 from pestuary.content import content_add
 
-# delete entries from previous tests
-try:
-    shutil.rmtree('/tmp/test-adddir')
-except:
-    pass
+from utils import cleanup_test_data_directory, setup_test_data_directory
 
-# create directories with files
-# tmp
-# ├── test-adddir
-# │   ├── subdir-1
-# │   ├── subdir-2
-# │   └── subdir-3
+if __name__ == '__main__':
+    directory = 'test-addir'
+    setup_test_data_directory(directory)
 
-os.mkdir('/tmp/test-adddir')
-os.mkdir('/tmp/test-adddir/subdir-1')
-os.mkdir('/tmp/test-adddir/subdir-2')
-os.mkdir('/tmp/test-adddir/subdir-3')
-dirlist = ['/tmp/test-adddir', '/tmp/test-adddir/subdir-1', \
-           '/tmp/test-adddir/subdir-2', '/tmp/test-adddir/subdir-3']
-for directory in dirlist:
-    for i in range(3):
-        f = open(directory + f"/file{i}.txt", "w")
-        random_content = ''.join(random.choice(string.printable) for _ in range(10))
-        f.write(random_content)
-        f.close()
+    responses, collection = content_add('/tmp/test-adddir', create_collection=True)
+    print(collection_list_content(collection.uuid))
 
-# add all the files under /tmp/test-adddir
-# responses = content_add('/tmp/test-adddir')
-
-# you can also create a dir-like structure
-# on estuary using collections
-responses, collection = content_add('/tmp/test-adddir', create_collection=True)
-print(collection_list_content(collection.uuid))
-
-# you can even list contents recursively!!
-# not sure what we want recursive vs non recursive to be
-# print(json.dumps(collection_list_content(collection["uuid"], "/", recursive=True), indent=4))
+    cleanup_test_data_directory(directory)
