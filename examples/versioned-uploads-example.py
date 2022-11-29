7d128f6bf26bb8de94057ffa369865289d52b47e
anjor <anjor@umd.edu>
2022-11-29 10:47:42 +0000

    new utils function

--- examples/versioned-uploads-example.py
+++ examples/versioned-uploads-example.py
@@ -1,8 +1,14 @@
-from pestuary import versioned_uploads
+import os
+
+from pestuary import utils, versioned_uploads
 
 if __name__ == '__main__':
     uploads = versioned_uploads.VersionedUploads()
+    name = 'e.txt'
+    url = 'https://shuttle-8.estuary.tech/gw/ipfs/bafkqaetbmjrwizlgbjtwq2lknmfgy3lon5ya/'
+    utils.download_dataset_from_url(name, url, data_directory='.')
 
-    response = uploads.add_with_version('a.txt')
+    response = uploads.add_with_version(name)
     print(response)
+    os.remove(name)
 
